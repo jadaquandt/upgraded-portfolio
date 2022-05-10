feb68a137b2ce94d30431626446cf332655bc812
Jada Quandt <jadaquandt@gmail.com>
2022-05-10 00:12:50 -0400

    First commit on new port

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,9 @@
+function Footer() {
+    return (
+      <div>
+         <p>This is the footer</p>
+      </div>
+    );
+  }
+  
+  export default Footer;
---
+++ src/components/Header/Header.js
@@ -0,0 +1,9 @@
+function Header() {
+    return (
+      <div>
+         <p>This is the header</p>
+      </div>
+    );
+  }
+  
+  export default Header;
