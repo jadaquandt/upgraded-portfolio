18453cf3453871599e4da49a2bb79ff2843c4ea5
Jada Quandt <jadaquandt@gmail.com>
2022-05-10 21:36:09 -0400

    Working on the footer

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
 import './App.css';
-import Footer from './components/Footer/Footer'
-import Header from './components/Header/Header';
-
+import Footer from './components/Footer/Footer';
+import Home from './containers/Home/Home';
+import About from './containers/About/About';
+import Resume from './components/Resume/Resume';
+import Projects from './components/Projects/Projects';
+import { Routes, Route } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-       <p>New Portfolio</p>
-       <Header/>
-       <Footer/>
-      </header>
+      <p>New Portfolio</p>
+      <Routes>
+        <Route path="/home" element={<Home />} />
+        <Route path="about" element={<About />} />
+        <Route path="projects" element={<Projects />} />
+        <Route path="resume" element={<Resume />} />
+      </Routes>
+      <Footer />
     </div>
   );
 }
--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,9 +1,63 @@
+import { Container, Row, Col } from 'react-bootstrap';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import TwitterIcon from '@mui/icons-material/Twitter';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import MailOutlineIcon from '@mui/icons-material/MailOutline';
+
 function Footer() {
-    return (
-      <div>
-         <p>This is the footer</p>
-      </div>
-    );
-  }
-  
-  export default Footer;
+  return (
+    <Container fluid className="footer">
+      <Row>
+        <Col className="footer-copywright">
+         <p>Portfolio created using React by Jada Quandt</p>
+        </Col>
+        <Col className="footer-body">
+          <ul className="footer-icons">
+            <li className="social-icons">
+              <a
+                href="https://github.com/jadaquandt"
+                style={{ color: "white" }}
+                target="_blank" 
+                rel="noopener noreferrer"
+              >
+                <GitHubIcon />
+              </a>
+            </li>
+            <li className="social-icons">
+              <a
+                href="https://twitter.com/jadaquandt"
+                style={{ color: "white" }}
+                target="_blank" 
+                rel="noopener noreferrer"
+              >
+                <TwitterIcon />
+              </a>
+            </li>
+            <li className="social-icons">
+              <a
+                href="https://www.linkedin.com/in/jadaquandt/"
+                style={{ color: "white" }}
+                target="_blank" 
+                rel="noopener noreferrer"
+              >
+                <LinkedInIcon />
+              </a>
+            </li>
+            <li className="social-icons">
+              <a
+                href="mailto:jadaquandt@gmail.com"
+                style={{ color: "white" }}
+                target="_blank" 
+                rel="noopener noreferrer"
+              >
+                <MailOutlineIcon />
+              </a>
+            </li>
+          </ul>
+        </Col>
+      </Row>
+    </Container>
+  );
+}
+
+export default Footer;
